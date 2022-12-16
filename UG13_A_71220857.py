9bd4d6167ea24da3b834d5390b22905ff54a4de4
dailyariella <118499517+dailyariella@users.noreply.github.com>
2022-12-16 13:22:53 +0700

    Update UG13_A_71220857.py

--- UG13_A_71220857.py
+++ UG13_A_71220857.py
@@ -1,69 +1,71 @@
 import turtle
 s = turtle.Screen()
 t = turtle.Turtle()
-# t.shape("turtle")
-s.bgcolor("gray")
-t.pensize(10)
-t.color("pink")
 
+#nama : Jesse Ariella Priska Nanlohy
+#nim  : 71220857
 #J857A
+
+#J
 t.penup()
-t.goto(-300,0)
+t.goto(-200,0)
 t.pendown()
-t.right(90)
-t.circle(45,180)
+t.pensize(25)
+t.color("blue")
+
 t.forward(100)
-t.left(90)
-t.forward(50)
+t.backward(200)
+t.forward(100)
+t.right(90)
+t.forward(150)
+t.circle(-60,200)
+t.penup()
 
 #8
-t.penup()
-t.goto(-150,30)
+t.goto(110,-80)
 t.pendown()
-t.circle(30)
-t.left(180)
-t.circle(30)
+t.pensize(25)
+t.color("blue")
+t.circle(60)
+t.penup()
+t.goto(110,-185)
+t.pendown()
+t.circle(60)
+t.penup()
 
 #5
-t.penup()
-t.goto(-95,-30)
+t.goto(225,-80)
 t.pendown()
-t.circle(40,180)
-t.right(90)
+t.right(-110)
+t.forward(60)
+t.left(90)
 t.forward(50)
-t.right(90)
-t.forward(40)
+t.left(90)
+t.forward(30)
+t.circle(-30,180)
+t.forward(30)
+t.penup()
 
 #7
-t.penup()
-t.goto(-30,-30)
-t.pendown()
-t.left(65)
-t.forward(140)
-t.left(115)
-t.forward(65)
-
 #A
-t.penup()
-t.goto(10,-30)
-t.pendown()
-t.right(180)
-t.left(65)
-t.forward(140)
-t.right(130)
-t.forward(140)
-t.left(180)
-t.penup()
-t.forward(70)
-t.pendown()
-t.left(65)
-t.forward(60)
-
-t.penup()
-t.goto(100,-30)
-t.pendown()
 
 
 
 
-s.exitonclick()
+
+
+
+
+
+
+
+s.exitonclick
+
+
+
+
+
+
+
+
+
